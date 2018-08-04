38b9c6ef26d11a2999692b50dd682de81430585e
llcc402 <llcc402@163.com>
2018-08-04 14:14:05 +0800

    first model

--- preprocessing.py
+++ preprocessing.py
@@ -8,12 +8,16 @@ Created on Fri Aug  3 14:51:19 2018
 #%% IMPORT PACKAGES
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+from sklearn.ensemble import RandomForestClassifier as RFC
+from sklearn import preprocessing
 
 #%% READ MAIN DATA
-data1 = pd.read_csv('~/Documents/Kaggle/application_train.csv',
+data1 = pd.read_csv('D:/KaggleCompetition/CreditMaster/Data/application_train.csv',
                     header = 0, 
                     index_col = 0)
+test = pd.read_csv('D:/KaggleCompetition/CreditMaster/Data/application_test.csv',
+                   header = 0,
+                   index_col = 0)
 
 #%% PROCESS MAIN DATA: fill obj NULLs with UNKNOWN
 obj_cols = data1.dtypes[data1.dtypes == 'object'].index
@@ -28,3 +32,26 @@ num_cols = data1.dtypes[data1.dtypes == 'float64'].index
 num_count_null_cols = np.sum(data1[num_cols].isnull())
 num_null_cols = num_count_null_cols[num_count_null_cols > 0].index
 
+# fill NULLs with -1
+data1[num_null_cols] = data1[num_null_cols].fillna(-1) 
+
+#%% FILL NULLS IN TEST DATA
+obj_cols = test.dtypes[test.dtypes == 'object'].index
+test[obj_cols] = test[obj_cols].fillna('UNKOWN')
+
+num_cols = test.dtypes[test.dtypes == 'float64'].index
+test[num_cols] = test[num_cols].fillna(-1)
+
+#%% LABEL ENCODER
+le = preprocessing.LabelEncoder()
+train_test = pd.concat([data1.iloc[:,1:], test], axis = 0)
+train_test[obj_cols] = train_test[obj_cols].apply(le.fit_transform)
+
+#%%
+training = train_test.iloc[:307511]
+test = train_test.iloc[307511:]
+y = data1['TARGET']
+
+#%% TRAIN A RANDOM FOREST
+rfc = RFC(n_estimators=500)
+model = rfc.fit(training, y)
