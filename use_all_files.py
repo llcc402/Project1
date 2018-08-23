d33ff3b2254d4d3960fa2654dc10c000e987dc4e
llcc402 <llcc402@163.com>
2018-08-23 23:01:29 +0800

    update use_all_files.py

--- use_all_files.py
+++ use_all_files.py
@@ -57,7 +57,7 @@ bureau_balance = pd.read_csv('bureau_balance.csv', index_col = 0)
 bureau = pd.get_dummies(bureau, dummy_na = True)
 bureau_balance = pd.get_dummies(bureau_balance, dummy_na = True)
 
-#%% feature engineering
+#%% feature engineering for bureau_balance
 
 # construct agg calcs
 bb_agg_calcs = defaultdict(list)
@@ -67,21 +67,67 @@ for c in bureau_balance.columns:
 
 bb_agg = bureau_balance.groupby(bureau_balance.index).agg(bb_agg_calcs)
 
+# rename column name, flattening multi-index column names 
+bb_agg.columns = [i[0] + '_' + i[1] for i in bb_agg.columns]
 
+# merge bb_agg with bureau
+bureau = bureau.join(bb_agg, how = 'left', on = 'SK_ID_BUREAU')
 
+#%% agg bureau
 
+num_columns = ['DAYS_CREDIT', 'CREDIT_DAY_OVERDUE',
+       'DAYS_CREDIT_ENDDATE', 'AMT_CREDIT_MAX_OVERDUE',
+       'CNT_CREDIT_PROLONG', 'AMT_CREDIT_SUM', 'AMT_CREDIT_SUM_DEBT',
+       'AMT_CREDIT_SUM_LIMIT', 'AMT_CREDIT_SUM_OVERDUE', 'DAYS_CREDIT_UPDATE',
+       'AMT_ANNUITY',  'MONTHS_BALANCE_min', 
+       'MONTHS_BALANCE_max', 'MONTHS_BALANCE_size']
 
+cat_columns = ['DAYS_ENDDATE_FACT', 'CREDIT_ACTIVE_Active', 'CREDIT_ACTIVE_Bad debt',
+       'CREDIT_ACTIVE_Closed', 'CREDIT_ACTIVE_Sold', 'CREDIT_ACTIVE_nan',
+       'CREDIT_CURRENCY_currency 1', 'CREDIT_CURRENCY_currency 2',
+       'CREDIT_CURRENCY_currency 3', 'CREDIT_CURRENCY_currency 4',
+       'CREDIT_CURRENCY_nan', 'CREDIT_TYPE_Another type of loan',
+       'CREDIT_TYPE_Car loan', 'CREDIT_TYPE_Cash loan (non-earmarked)',
+       'CREDIT_TYPE_Consumer credit', 'CREDIT_TYPE_Credit card',
+       'CREDIT_TYPE_Interbank credit',
+       'CREDIT_TYPE_Loan for business development',
+       'CREDIT_TYPE_Loan for purchase of shares (margin lending)',
+       'CREDIT_TYPE_Loan for the purchase of equipment',
+       'CREDIT_TYPE_Loan for working capital replenishment',
+       'CREDIT_TYPE_Microloan', 'CREDIT_TYPE_Mobile operator loan',
+       'CREDIT_TYPE_Mortgage', 'CREDIT_TYPE_Real estate loan',
+       'CREDIT_TYPE_Unknown type of loan', 'CREDIT_TYPE_nan',
+       'MONTHS_BALANCE_mean', 'STATUS_0_mean', 'STATUS_1_mean',
+       'STATUS_2_mean', 'STATUS_3_mean', 'STATUS_4_mean', 'STATUS_5_mean',
+       'STATUS_C_mean', 'STATUS_X_mean', 'STATUS_nan_mean']
 
+num_agg_calcs = {'DAYS_CREDIT': ['min', 'max', 'mean', 'var'],
+                 'CREDIT_DAY_OVERDUE':['mean', 'max'], 
+                 'DAYS_CREDIT_ENDDATE':['max', 'min', 'mean'],
+                 'AMT_CREDIT_MAX_OVERDUE':['max', 'mean'],
+                 'CNT_CREDIT_PROLONG':['sum'],
+                 'AMT_CREDIT_SUM':['max', 'mean', 'sum'],
+                 'AMT_CREDIT_SUM_DEBT':['max', 'mean', 'sum'],
+                 'AMT_CREDIT_SUM_LIMIT':['mean', 'sum'],
+                 'AMT_CREDIT_SUM_OVERDUE':['max', 'mean'],
+                 'DAYS_CREDIT_UPDATE':['mean'],
+                 'AMT_ANNUITY':['max', 'mean'],
+                 'MONTHS_BALANCE_min':['min'],
+                 'MONTHS_BALANCE_max':['max'],
+                 'MONTHS_BALANCE_size':['sum', 'mean']}
 
+cat_agg_calcs = dict()
+for c in cat_columns:
+    cat_agg_calcs[c] = ['mean']
 
+bureau_agg_calcs = {**num_agg_calcs, **cat_agg_calcs}
 
+bureau_agg = bureau.groupby('SK_ID_BUREAU').agg(bureau_agg_calcs)
 
+# rename column name
+bureau_agg.columns = [c[0] + '_' + c[1] for c in bureau_agg.columns]
 
-
-
-
-
-
+#%% 
 
 
 
